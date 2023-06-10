9e0b65ef5c5788333dd3d1817433aaf8d4e795c6
RamiroGhise <rami.ghise@gmail.com>
2023-06-10 12:21:03 +0300

    Add redux reducer test to SuccessfullyLoadVenuesAction action

--- test/redux_test.dart
+++ test/redux_test.dart
@@ -1,11 +1,12 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
-import 'package:redux/redux.dart';
 import 'package:venues/redux/actions.dart';
 import 'package:venues/redux/app_state.dart';
 import 'package:venues/redux/reducer.dart';
 import 'package:venues/services/location/location_exceptions.dart';
+import 'package:venues/services/location/venue.dart';
 
 void main() {
   group('Testing reducer', () {
@@ -28,7 +29,8 @@ void main() {
         error: null,
       );
       const initialState = isLoadingState;
-      final action = FailedLoadVenuesAction(error: BadRequestLocationException('bad request'));
+      final action = FailedLoadVenuesAction(
+          error: BadRequestLocationException('bad request'));
       final AppState state = reducer(initialState, action);
 
       final failedLoadingState = AppState(
@@ -41,6 +43,38 @@ void main() {
       expect(state.venues, failedLoadingState.venues);
       expect(state.error, failedLoadingState.error);
       expect(state, isNot(failedLoadingState));
+    });
+
+    test('testing response to SuccessfullyLoadVenuesAction', () {
+      const isLoadingState = AppState(
+        isLoading: true,
+        venues: null,
+        error: null,
+      );
+      const initialState = isLoadingState;
+
+      final venuesMock = File('test/venues.json');
+      final venuesJson = jsonDecode(venuesMock.readAsStringSync());
+      var results = venuesJson['results'];
+      List<Venue> venuesList = [];
+      for (final venueJson in results) {
+        final venue = Venue.fromFoursquare(venueJson);
+        venuesList.add(venue);
+      }
+
+      final action = SuccessfullyLoadVenuesAction(venues: venuesList);
+      final state = reducer(initialState, action);
+
+      final successfullyLoadVenuesState = AppState(
+        isLoading: false,
+        venues: state.venues,
+        error: null,
+      );
+
+      expect(state.isLoading, successfullyLoadVenuesState.isLoading);
+      expect(state.venues, successfullyLoadVenuesState.venues);
+      expect(state.error, successfullyLoadVenuesState.error);
+      expect(state, isNot(successfullyLoadVenuesState));
     });
   });
 }
